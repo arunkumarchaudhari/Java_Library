1aa329ec55e5bb8656887f00697815dae4ed57a7
Arun Kumar Chaudhari <arunchaudhari12379@gmail.com>
2022-08-10 23:57:13 +0530

    Add files via upload
    Header for arunlib.jar file

---
+++ arunlib.h
@@ -0,0 +1,17 @@
+int MaxOfTwoNum(int a, int b);
+int MaxOfThreeNum(int a, int b, int c);
+int MaxOfFourNum(int a, int b,int c,int d);
+int sum(int ... arr);
+void Factorial (int a);
+void FibonacciSeries(int a);
+void PalindromNumber(int a);
+bool PrimeOrNot(int a);
+int Power(int a,int b);
+void MatrixAdd(int r,int c);
+void MatrixMult(int a, int b);
+void IsAnagram(String m, String n);
+void isPalindrom_string(String a);
+
+
+
+
